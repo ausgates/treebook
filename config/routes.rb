1434690050ce78af2e6030c38fce145686e40656
Austin Gates <Austin@austins-imac.attlocal.net>
2015-04-19 04:09:34 -0400

    Added a sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :statuses
+  root to: 'statuses#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
