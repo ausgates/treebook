e7892dec114e77db3bdff36a50b821a196699007
Austin Gates <Austin@austins-imac.attlocal.net>
2015-04-19 21:35:41 -0400

    Added user ID to statuses:

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419073809) do
+ActiveRecord::Schema.define(version: 20150420013252) do
 
   create_table "statuses", force: :cascade do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
