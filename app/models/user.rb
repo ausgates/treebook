a132fbbfa93412f222dbd5c375efeae755d196fe
Austin Gates <Austin@austins-imac.attlocal.net>
2015-04-20 01:59:18 -0400

    shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   #attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  def full_name
+  	first_name + " " + last_name
+  end
 end
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
+ 
 end
